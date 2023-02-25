ea503031ad543c81b703c5532315a6bd9b86c14d
Brooks Ryan <brooksryan19@gmail.com>
2023-02-24 17:40:42 -0800

    Update waitress-server.py

--- waitress-server.py
+++ waitress-server.py
@@ -5,4 +5,4 @@ import os
 BROOKSPT3_HOST = os.environ.get('BROOKSPT3_HOST')
 BROOKSPT3_PORT = os.environ.get('BROOKSPT3_PORT')
 OPENAPI_KEY = os.environ.get('OPENAPI_KEY')
-serve(app.app, host='localhost', port=BROOKSPT3_PORT)
+serve(app.app, host=BROOKSPT3_HOST, port=BROOKSPT3_PORT)
